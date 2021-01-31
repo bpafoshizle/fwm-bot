23c3bee0691299f37f91f18e57e12700d061e260
bpafoshizle <bpafoshizle@gmail.com>
2021-01-31 15:07:33 -0600

    added scripts for secret for ghcr

---
+++ kube/create-ghcr-secret.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+kubectl --kubeconfig ~/.kube/bletchley-config \
+    -n egroup-bot \
+    create secret docker-registry ghcr-egroup-bot \
+        --docker-server=ghcr.io \
+        --docker-username=${GITHUB_USERNAME} \
+        --docker-password=${GITHUB_PACKAGE_TOKEN}  \
+        --docker-email=${GITHUB_EMAIL}
