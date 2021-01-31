f2c5fcc50e9bf58d923fe17c5bfcbca8b72a7a9f
bpafoshizle <bpafoshizle@gmail.com>
2021-01-31 14:43:45 -0600

    added shebang to bash scripts

--- kube/create-namespace.sh
+++ kube/create-namespace.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 kubectl --kubeconfig ~/.kube/bletchley-config create namespace egroup-bot
--- kube/deploy.sh
+++ kube/deploy.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 kubectl --kubeconfig ~/.kube/bletchley-config create -f egroup-bot-deployment.yml
