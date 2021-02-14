2b9b14960a535bb7a8d4d3392f3033a9a3398e00
bpafoshizle <bpafoshizle@gmail.com>
2021-02-13 21:04:49 -0600

    stock cog with ability to get news and stock price info

---
+++ app/cogs/stocks.py
@@ -0,0 +1,173 @@
+import json
+import logging
+import os
+from datetime import datetime
+
+import aiohttp
+import discord
+from bs4 import BeautifulSoup
+from cogs.utils.timing import calc_tomorrow_4pm, calc_tomorrow_7am, wait_until
+from discord.ext import commands, tasks
+
+polygon_token = os.getenv("POLYGON_TOKEN")
+
+
+class StockQuote(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.stock_list = [
+            "SPY",
+            "QQQ",
+            "GME",
+            "IJR",
+            "BTC-USD",
+            "ETC-USD",
+        ]
+        # pylint: disable=no-member
+        # self.morning_stocks_task.start()
+        # pylint: disable=no-member
+        # self.afternoon_stocks_task.start()
+
+    @commands.command()
+    async def stockquote(self, ctx, symbol):
+        stock_quote = self.formatLatestStockQuoteEmbed(
+            *await self.getLatestStockQuote(symbol)
+        )
+        await ctx.send(embed=stock_quote)
+
+    @commands.command()
+    async def stockclose(self, ctx, symbol):
+        stock_close = self.formatPrevCloseEmbed(
+            *await self.getPrevClose(symbol.upper())
+        )
+        await ctx.send(embed=stock_close)
+
+    @commands.command()
+    async def stocknews(self, ctx, symbol):
+        stock_news = self.formatStockNewsEmbed(
+            await self.getStockNewsMarketWatch(symbol)
+        )
+        for article in stock_news:
+            logging.debug(article)
+            await ctx.send(embed=article)
+
+    async def getStockNewsPolygon(self, symbol):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                f"https://api.polygon.io/v1/meta/symbols/{symbol}/news?perpage=5&page=1&apiKey={polygon_token}"
+            ) as r:
+                if r.status == 200:
+                    news = await r.json()
+                    logging.debug(news)
+                    return news
+
+    async def getStockNewsMarketWatch(self, symbol):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                f"https://www.marketwatch.com/investing/stock/{symbol}?mod=quote_search"
+            ) as r:
+                if r.status == 200:
+                    soup = BeautifulSoup(await r.text(), features="html.parser")
+                    soup = soup.find_all("div", attrs={"class": "element--article"})[:5]
+                    news = []
+                    for section in soup:
+                        article = {}
+                        article["timestamp"] = getattr(
+                            section.find("span", class_="article__timestamp"),
+                            "string",
+                            datetime.now(),
+                        )
+                        article["source"] = getattr(
+                            section.find("span", class_="article__author"),
+                            "string",
+                            "by Unknown",
+                        )
+                        if "no-image" not in section["class"]:
+                            article["title"] = section.find(
+                                "h3", class_="article__headline"
+                            ).a.string.strip()
+                            article["image"] = (
+                                section.find("a", class_="figure__image")
+                                .img["data-srcset"]
+                                .split(",")[2]
+                                .split()[0]
+                            )
+                            article["url"] = section.find("a", class_="figure__image")[
+                                "href"
+                            ]
+                        else:
+                            article["title"] = section.find(
+                                "h3", class_="article__headline"
+                            ).span.string.strip()
+                        news.append(article)
+
+                    return news
+
+    async def getPrevClose(self, symbol):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                f"https://api.polygon.io/v2/aggs/ticker/{symbol}/prev?unadjusted=true&apiKey={polygon_token}"
+            ) as r:
+                if r.status == 200:
+                    json_data = await r.json()
+                    logging.debug(json_data)
+                    prev_close = json_data["results"][0]["c"]
+                    prev_high = json_data["results"][0]["h"]
+                    prev_low = json_data["results"][0]["l"]
+                    return (symbol, prev_close, prev_high, prev_low)
+
+    async def getLatestStockQuote(self, symbol):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                f"https://finance.yahoo.com/quote/{symbol}?p={symbol}"
+            ) as r:
+                if r.status == 200:
+                    logging.debug(
+                        f"received response from https://finance.yahoo.com/quote/{symbol}?p={symbol}"
+                    )
+                    soup = BeautifulSoup(await r.text(), features="html.parser")
+                    soup = soup.find(id="quote-header-info")
+                    name = soup.h1.text
+                    lastprice = soup.find(
+                        "span", class_="Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"
+                    ).string
+                    change = soup.find("span", attrs={"class": "Fw(500)"}).string
+                    quotetime = soup.find("div", id="quote-market-notice").string
+
+                    return (symbol, name, lastprice, change, quotetime)
+
+    def formatLatestStockQuoteEmbed(self, symbol, name, lastprice, change, quotetime):
+        embed = discord.Embed(
+            title="Stock Latest Quote",
+            description="Most recent quote from your friendly Egroup bot",
+            color=0x9D2235,
+        )
+        embed.add_field(name="Symbol", value=symbol)
+        embed.add_field(name="Name", value=name)
+        embed.add_field(name="Quote time", value=quotetime, inline=False)
+        embed.add_field(name="Last Price", value=lastprice)
+        embed.add_field(name="Change", value=change)
+        return embed
+
+    def formatPrevCloseEmbed(self, symbol, prev_close, prev_high, prev_low):
+        embed = discord.Embed(
+            title="Stock Previous Close",
+            description="Previous close info from your friendly Egroup bot",
+            color=0x9D2235,
+        )
+        embed.add_field(name="Symbol", value=symbol, inline=False)
+        embed.add_field(name="Close", value=prev_close)
+        embed.add_field(name="High", value=prev_high)
+        embed.add_field(name="Low", value=prev_low)
+        return embed
+
+    def formatStockNewsEmbed(self, news):
+        embeds = []
+        for article in news:
+            embed = discord.Embed(
+                title=article["title"], url=article.get("url", ""), color=0x9D2235
+            )
+            embed.set_image(url=article.get("image", ""))
+            embed.add_field(name="Source", value=article["source"])
+            embeds.append(embed)
+        return embeds
--- app/cogs/utils/timing.py
+++ app/cogs/utils/timing.py
@@ -12,9 +12,23 @@ async def wait_until(dt):
     await asyncio.sleep((dt - now).total_seconds())
 
 
+def calc_tomorrow_at_time(hour, min):
+    """Calculate tomorrow in US Central time. Convert to UTC. Return.
+    Arguments are hour in military (24hr) time, min
+    """
+    tmwr_ct = datetime.now(us_central_tz) + timedelta(days=1)
+    tmwr_ct = tmwr_ct.replace(hour=hour, minute=min, second=0, microsecond=0)
+    tmrw_utc = tmwr_ct.astimezone(timezone.utc)
+    return tmrw_utc
+
+
 def calc_tomorrow_6am():
-    """ Calculate tomorrow 6am in US Central time. Convert to UTC. Return. """
-    tmrw_6am_ct = datetime.now(us_central_tz) + timedelta(days=1)
-    tmrw_6am_ct = tmrw_6am_ct.replace(hour=6, minute=0, second=0, microsecond=0)
-    tmrw_6am_utc = tmrw_6am_ct.astimezone(timezone.utc)
-    return tmrw_6am_utc
+    return calc_tomorrow_at_time(6, 0)
+
+
+def calc_tomorrow_7am():
+    return calc_tomorrow_at_time(7, 0)
+
+
+def calc_tomorrow_4pm():
+    return calc_tomorrow_at_time(16, 0)
