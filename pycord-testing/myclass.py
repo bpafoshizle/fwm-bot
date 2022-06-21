d185746b436552b17616aabc980329232bc13fb9
bpafoshizle <bpafoshizle@gmail.com>
2022-06-21 06:04:19 -0500

    black linted py-cord testing

--- pycord-testing/myclass.py
+++ pycord-testing/myclass.py
@@ -22,22 +22,14 @@ class MyClass(commands.Cog):
         await ctx.respond("Hello from my_command_method!")
 
     @commands.slash_command()
-    async def my_slash_command(
-        self,
-        ctx,
-        duration = 5
-    ):
+    async def my_slash_command(self, ctx, duration=5):
         await ctx.respond("Please wait")
         time.sleep(duration)
         await ctx.edit("Hi, this is my_slash_command")
 
     @commands.slash_command()
-    async def my_second_slash_command(
-        self,
-        ctx
-    ):
+    async def my_second_slash_command(self, ctx):
         await ctx.respond("Hi, this is my_second_slash_command")
-
 
 
 if __name__ == "__main__":
