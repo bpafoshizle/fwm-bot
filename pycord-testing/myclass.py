773db8fb8e877fe32d1c2a692e7bdde26ec4e2f5
bpafoshizle <bpafoshizle@gmail.com>
2022-06-21 05:51:35 -0500

    updated pydiscogs library to use debug_guilds, need to rebuild image

--- pycord-testing/myclass.py
+++ pycord-testing/myclass.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import time
 
 from discord.ext import commands
 
@@ -20,8 +21,26 @@ class MyClass(commands.Cog):
         logger.debug(f"reason for the dorukyumParam: {dorukyumParam}")
         await ctx.respond("Hello from my_command_method!")
 
+    @commands.slash_command()
+    async def my_slash_command(
+        self,
+        ctx,
+        duration = 5
+    ):
+        await ctx.respond("Please wait")
+        time.sleep(duration)
+        await ctx.edit("Hi, this is my_slash_command")
+
+    @commands.slash_command()
+    async def my_second_slash_command(
+        self,
+        ctx
+    ):
+        await ctx.respond("Hi, this is my_second_slash_command")
+
+
 
 if __name__ == "__main__":
-    bot = commands.Bot()
+    bot = commands.Bot(debug_guilds=[os.getenv("DISCORD_GUILD_ID")])
     bot.add_cog(MyClass(bot, [os.getenv("DISCORD_GUILD_ID")]))
     bot.run(os.getenv("DISCORD_TOKEN"))
